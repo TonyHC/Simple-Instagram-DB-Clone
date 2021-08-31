a39f5f10164dc0ba98e624341a49a06c22761e4b
Tony Chao <tchao@mail.usf.edu>
2021-08-31 14:03:20 -0400

    Photos Table Schema

---
+++ Instagram-Clone/Photos/Photos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    # caption VARCHAR(255) DEFAULT '',
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES Users(id) ON DELETE CASCADE
+);
