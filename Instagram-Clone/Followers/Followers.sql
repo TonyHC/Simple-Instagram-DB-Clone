dddfc306abc92a2b9d10bf970833ec066bac7964
Tony Chao <tchao@mail.usf.edu>
2021-08-31 20:23:44 -0400

    Added ON DELETE CASCADE to Follows Table Schema

--- Instagram-Clone/Followers/Followers.sql
+++ Instagram-Clone/Followers/Followers.sql
@@ -2,7 +2,7 @@ CREATE TABLE Follows (
     follower_id INT NOT NULL,
     followee_id INT NOT NULL,
     followed_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(follower_id) REFERENCES Users(id),
-    FOREIGN KEY(followee_id) REFERENCES Users(id),
+    FOREIGN KEY(follower_id) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY(followee_id) REFERENCES Users(id) ON DELETE CASCADE,
     PRIMARY KEY(follower_id, followee_id)
 );
